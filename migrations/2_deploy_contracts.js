b71e53c06c674ab6fd7663b9623bd9d634963736
merlox <merunasgrincalaitis@gmail.com>
2020-04-21 17:30:21 +0200

    Fixed deployment process in 2_deploy_contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,10 @@ const OVRToken = artifacts.require("OVRToken")
 const TokenBuy = artifacts.require("TokenBuy")
 const ICO = artifacts.require("ICO")
 
-module.exports = async deployer => {
-    const ovrToken = await deployer.deploy(OVRToken)
-    console.log('Ovr token', ovrToken)
+module.exports = deployer => {
     deployer.deploy(OVRLand)
-    await deployer.deploy(TokenBuy, ovrToken.address)
     deployer.deploy(ICO)
+    deployer.deploy(OVRToken).then(ovrToken => {
+        deployer.deploy(TokenBuy, ovrToken.address)
+    })
 }
