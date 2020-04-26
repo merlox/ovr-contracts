c4a2658eb389c86be79184d1f804f0b06a214a0a
merlox <merunasgrincalaitis@gmail.com>
2020-04-26 12:45:34 +0200

    Starting working on ICO tests

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.5.0;
 
 import '@openzeppelin/contracts/math/SafeMath.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import '@openzeppelin/contracts/lifecycle/Pausable.sol';
 // Custom modified version to use String tokenIds instead of uints
-import './ERC721.sol';
 
 contract Ownable {
     address payable public owner;
@@ -45,6 +45,8 @@ contract ICO is Ownable, Pausable {
         uint256 paid;
         uint256 lastBidTimestamp;
         AuctionState state;
+        uint256 cashbackAmount;
+        bool isCashbackRedeemed;
 
         // Marketplace functionality
         uint256 sellPrice;
@@ -65,8 +67,9 @@ contract ICO is Ownable, Pausable {
     event AuctionBid(address indexed newBidder, address indexed oldBidder, uint256 indexed landToBuy, uint256 paid, uint256 timestamp);
     event WonLand(address indexed winner, uint256 indexed landId, uint256 price);
     event LandSaleStarted(address indexed owner, uint256 indexed landId, uint256 price);
+    event CashbackRedeemed(uint256 indexed landId, address indexed receiver, uint256 amount, uint256 timestamp);
     // For lands that have been on sale but were removed by the owner
-    event LandSaleCancelled(address indexed owner, uint256 indexed landId, uint256 price);
+    event LandSaleCancelled(address indexed owner, uint256 indexed landId);
     event LandOfferCreated(uint256 indexed id, address indexed by, uint256 indexed landId, uint256 price, uint256 timestamp, uint256 expirationDate);
     event LandSold(uint256 indexed landId, address indexed oldOwner, address indexed buyer, uint256 price, uint256 timestamp);
     event LandOfferDeclined(uint256 indexed landOfferId, uint256 indexed landId);
@@ -115,13 +118,14 @@ contract ICO is Ownable, Pausable {
     /// TODO test that the auctions[] is actually being updated
     /// To participate in a new or existing auction
     /// The user must first approve the right amount of OVR tokens to execute this
-    /// @return bool If you were able to participate in the auction correctly or not
     /// False when the auction has ended and True when you've participated successfully
-    function participateInAuction(uint256 _landId) public whenNotPaused returns(bool) {
+    function participateInAuction(uint256 _landId) public whenNotPaused {
         require(checkEpoch(_landId), "This land isn't available at the current epoch");
 
         Land storage landToBuy = lands[_landId];
         uint256 allowance = IERC20(ovrToken).allowance(msg.sender, address(this));
+
+        require(now.sub(landToBuy.lastBidTimestamp) >= 24 hours, 'This land auction has ended');
 
         if (landToBuy.state == AuctionState.ACTIVE) {
             // The auction for this land ID has been started
@@ -140,29 +144,28 @@ contract ICO is Ownable, Pausable {
             landToBuy.lastBidTimestamp = now;
             extractableTokens = extractableTokens.add(oldBid);
             emit AuctionBid(msg.sender, oldBidder, _landId, nextBid, now);
-            return true;
         } else if (landToBuy.state == AuctionState.NOT_STARTED) {
             // This is a new auction
             // Check the tokens locked in active auctions because it may happen that he has 
             // 10 approved and wants to participate in 5 auctions check that he has enough
             // to cover this auction too
             require(allowance >= initialLandBid, 'Your allowance must equal or exceed the cost of participating in this auction');
-            lands[_landId] = Land(msg.sender, _landId, initialLandBid, now, AuctionState.ACTIVE, 0, false);
+            lands[_landId] = Land(msg.sender, _landId, initialLandBid, now, AuctionState.ACTIVE, 0, false, 0, false);
             activeLands.push(_landId);
             emit AuctionStarted(msg.sender, _landId, initialLandBid, now);
-            return true;
         } else {
-            return false;
+            revert('The auction has ended for this land');
         }
     }
 
     /// To redeem the land that you won in an auction
     function redeemWonLand(uint256 _landId) public whenNotPaused {
-        Land memory auction = lands[_landId];
-        if (now.sub(auction.lastBidTimestamp) > 24 hours) {
+        Land storage land = lands[_landId];
+        if (now.sub(land.lastBidTimestamp) > 24 hours) {
             lands[_landId].state = AuctionState.ENDED;
         }
-        require(auction.state != AuctionState.ENDED, "You can't redeem this land until its auction is finished");
+        require(land.state != AuctionState.ENDED, "You can't redeem this land until its auction is finished");
+        require(land.owner == msg.sender, 'You must be the land winner to redeem it');
         uint256 cashbackPercentage;
         uint256 monthPurchasedSinceBeginning = now.sub(contractCreationDate).div(30 days);
         if (monthPurchasedSinceBeginning == 1) {
@@ -186,22 +189,31 @@ contract ICO is Ownable, Pausable {
         } else {
             cashbackPercentage = 0;
         }
-        uint256 cashback = auction.paid.mul(cashbackPercentage).div(100);
+        uint256 cashback = land.paid.mul(cashbackPercentage).div(100);
         cashbacks[msg.sender] = cashbacks[msg.sender].add(cashback);
+        land.cashbackAmount = cashback;
 
         // Transfer the land to the user
-        // TODO Test this to make sure that the parameters are correct cuz it may need to be changed I'm not sure how it works
         IERC721(ovrLand).safeTransferFrom(owner, msg.sender, _landId);
         ownedLands[msg.sender].push(_landId);
 
-        emit WonLand(msg.sender, _landId, auction.paid);
+        emit WonLand(msg.sender, _landId, land.paid);
     }
 
     /// To get your cashback for the buyers in the initial 9 months
-    function redeemCashback() public whenNotPaused {
-        uint256 tempCashback = cashbacks[msg.sender];
-        cashbacks[msg.sender] = 0;
-        IERC20(ovrToken).transfer(msg.sender, tempCashback);
+    /// @param _landId The land whose cashback you want to get
+    function redeemCashback(uint256 _landId) public whenNotPaused {
+        Land storage land = lands[_landId];
+        require(!land.isCashbackRedeemed, 'The cashback has already been redeemed for this land');
+        require(land.owner == msg.sender, 'You must be the land owner to redeem its cashback');
+        require(now.sub(land.lastBidTimestamp) >= 30 days, "You can't redeem a cashback before 30 days");
+        uint256 tempAmount = land.cashbackAmount;
+        cashbacks[msg.sender] = cashbacks[msg.sender].sub(land.cashbackAmount);
+        land.isCashbackRedeemed = true;
+        land.cashbackAmount = 0;
+
+        IERC20(ovrToken).transfer(msg.sender, tempAmount);
+        emit CashbackRedeemed(_landId, msg.sender, land.cashbackAmount, now);
     }
 
     /// To extract the tokens that may have been sent to this contract by accident
@@ -217,17 +229,17 @@ contract ICO is Ownable, Pausable {
     /// To put on sell a land you own
     /// Note: the price can be 0 to give it away for free
     /// @param _onSale To indicate whether you want to put it on sale or remove it from the sale
-    function sellLand(uint256 _landId, uint256 _price, bool _onSale) public whenNotPaused {
+    function putLandOnSale(uint256 _landId, uint256 _price, bool _onSale) public whenNotPaused {
         Land storage land = lands[_landId];
         require(msg.sender == land.owner, 'You must be the land owner to put it on sale');
         require(land.state == AuctionState.ENDED, 'The land auction must have been completed to put it on sale');
-        landsOnSaleOrSold.push(landId);
+        landsOnSaleOrSold.push(_landId);
         land.onSale = _onSale;
         land.sellPrice = _price;
         if (_onSale) {
-            emit LandSaleStarted(land.owner, _landId, true);
+            emit LandSaleStarted(land.owner, _landId, _price);
         } else {
-            emit LandSaleCancelled(land.owner, _landId, false);
+            emit LandSaleCancelled(land.owner, _landId);
         }
     }
 
@@ -259,6 +271,7 @@ contract ICO is Ownable, Pausable {
         uint256 allowance = IERC20(ovrToken).allowance(msg.sender, address(this));
         require(land.state == AuctionState.ENDED, 'The land auction must have been completed to send the offer to buy it');
         require(allowance >= _price, 'You must approve the right amount of OVR tokens to offer to buy it');
+        require(_expirationDate > now, 'The expiration date must be larger than now');
 
         LandOffer memory newOffer = LandOffer(lastLandOfferId, msg.sender, _landId, _price, now, _expirationDate, LandOfferState.ACTIVE);
         landOffers[lastLandOfferId] = newOffer;
@@ -276,6 +289,7 @@ contract ICO is Ownable, Pausable {
         require(landOffer.state == LandOfferState.ACTIVE, 'The offer must be active to be able to respond to it');
         require(landOffer.expirationDate > now, 'The offer is expired');
         require(land.state == AuctionState.ENDED, 'The auction state must be ended to accept the offer');
+        require(land.owner == msg.sender, 'You must be the owner to accept the land offer');
         if (_accept) {
             emit LandSold(land.landToBuy, land.owner, landOffer.by, landOffer.price, now);
             landOffer.state = LandOfferState.ACCEPTED;
@@ -283,7 +297,7 @@ contract ICO is Ownable, Pausable {
             land.onSale = false;
             land.sellPrice = 0;
             IERC20(ovrToken).transferFrom(landOffer.by, land.owner, landOffer.price);
-            IERC721(ovrLand).safeTransferFrom(land.owner, msg.sender, _landId);
+            IERC721(ovrLand).safeTransferFrom(land.owner, msg.sender, land.landToBuy);
         } else {
             landOffer.state = LandOfferState.DECLINED;
             emit LandOfferDeclined(_landOfferId, land.landToBuy);
@@ -292,16 +306,8 @@ contract ICO is Ownable, Pausable {
 
     /// Returns an array with the the landOfferIds for a given land id
     /// you can check each independently using the array ownedLands
-    function checkMyLandOffers(uint256 _landId) public view returns(uint256[] results) {
-        uint256[] memory results;
-        uint256 counter = 0;
-        for(uint256 i = 0; i < landOffers.length; i++) {
-            if (landOffers[i].landId == _landId) {
-                results[counter] = landOffers[i].id;
-                counter++;
-            }
-        }
-        return results;
+    function checkMyLandOffers(uint256 _landId) public view returns(uint256[] memory) {
+        return landOfferIds[_landId];
     }
 
     /// Returns the landIds you won so you know which landIds you can redeem
@@ -319,9 +325,9 @@ contract ICO is Ownable, Pausable {
     }
 
     /// Checks if the token you want to buy is within the epoch available
-    /// @returns bool True if it's in aA valid epoch and false if not
+    /// @return bool True if it's in aA valid epoch and false if not
     function checkEpoch(uint256 _landId) public view returns(bool) {
-        uint256 currentMonth = now.minus(contractCreationDate).div(30);
+        uint256 currentMonth = now.sub(contractCreationDate).div(30);
         // Extract the last 2 digits
         uint256 landIdDigits = _landId % 100;
         if (currentMonth == 1) {
--- test/ico.test.js
+++ test/ico.test.js
@@ -1,8 +1,25 @@
-// To test bytes32 functions
-function fillBytes32WithSpaces(name) {
-    let nameHex = web3.utils.toHex(name)
-    for(let i = nameHex.length; i < 66; i++) {
-        nameHex = nameHex + '0'
-    }
-    return nameHex
-}
+const assert = require('assert')
+const ICO = artifacts.require('ICO')
+const OVRToken = artifacts.require('OVRToken')
+const OVRLand = artifacts.require('OVRLand')
+let ovrToken = {}
+let ovrLand = {}
+let ico = {}
+let accounts = {} // Global accounts
+const priceToSetEth = 100
+const priceToSetUsd = 10
+
+contract('ICO', accs => {
+    accounts = accs
+
+    beforeEach(async () => {
+        ovrToken = await OVRToken.new()
+        ovrLand = await OVRLand.new()
+        ico = await ICO.new(ovrToken.address, ovrLand.address, 10e18)
+    })
+
+    it('should set the OVR token, land and initial land bid successfully', async () => {
+        ico = await ICO.new(ovrToken.address, ovrLand.address, 10e18)
+        console.log('expect', expect)
+    })
+})
